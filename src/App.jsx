a558e3b518e412e5d87439618af2b66ac72bdc6c
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-26 12:39:15 +0400

    Basic Styled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './components/Button/Button'
 
 function App() {
   
   return (
     <>
-
+    <div>
+    <Button>Styled Button</Button>
+    </div>
     </>
   )
 }
