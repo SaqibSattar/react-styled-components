76f5715031675929c266dfee20b545ec866dfd16
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-26 15:28:16 +0400

    Theming and Global Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 import StyledButton, { FancyButton, SubmitButton } from './components/Button/Button'
-import { AnimatedLogo } from './components/Button/Button.styles'
+import { AnimatedLogo, DarkButton } from './components/Button/Button.styles'
 import reactLogo from './assets/react.svg'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+const theme = {
+  dark: {
+    primary: '#000',
+    text: '#fff'
+  },
+  light: {
+    primary: '#fff',
+    text: '#000'
+  },
+  fontFamily: 'Segoe UI'
+}
+
+const GlobalStyle = createGlobalStyle`
+  button {
+    font-family: ${props => props.theme.fontFamily};
+  }
+`
 
 function App() {
   
   return (
     <>
+    <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <div>
     <StyledButton>Styled Button</StyledButton>
     <div>
@@ -24,6 +45,8 @@ function App() {
     <SubmitButton>Submit</SubmitButton>
     <AnimatedLogo src={reactLogo} />
     </div>
+    <DarkButton>Dark Button</DarkButton>
+    </ThemeProvider>
     </>
   )
 }
